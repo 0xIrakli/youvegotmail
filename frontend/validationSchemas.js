c864ce07962eb26160814e4b3ed4644a835a0b0a
0xIrakli <butu.irakli@gmail.com>
2024-07-31 12:56:30 +0400

    fix register password show

--- frontend/validationSchemas.js
+++ frontend/validationSchemas.js
@@ -20,10 +20,9 @@ export const registerSchema = object({
 		.matches(/^\S+@\S+\.\S+$/, 'Please enter valid Email address')
 		.required('Email is required'),
 	password: string().trim().min(8).max(20).required('Password is required'),
-
-	confirmPassword: string().when(
-		'password',
-		(password, schema) => string().equals(password).required('Password is required'),
-		'passwords dont match'
+	confirmPassword: string().when('password', (password, schema) =>
+		string()
+			.equals(password, 'passwords dont match')
+			.required('Password is required')
 	),
 })
